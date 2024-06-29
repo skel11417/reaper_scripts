4b8ba945fd48e048372d2fe9c7b669268edde171
skel11417 <shonkelli@gmail.com>
2024-06-29 08:01:41 -0400

    Update add_pro-q2.lua
    Modified to open the instance of pro-q2 if one exists

--- add_pro-q2.lua
+++ add_pro-q2.lua
@@ -2,7 +2,7 @@ function Main()
   reaper.Undo_BeginBlock()
   fxname = "Fabfilter Pro-Q 2"
   track =  reaper.GetSelectedTrack(0, 0)
-  reaper.TrackFX_AddByName( track, fxname, 0, -1 ) --add volume plugin
+  reaper.TrackFX_AddByName( track, fxname, 0, 1 ) --add pro-q plugin instance if one does not exist
   if reaper.GetMediaTrackInfo_Value(track, "I_FXEN") <= 0 then
       reaper.SetMediaTrackInfo_Value( track, "I_FXEN", 1 )
   end
